50f864b4512d7a2c97db672ecffbc93e7a3dd318
BenGaraffa <ben.garaffa@gmail.com>
2022-04-19 19:21:07 +0100

    Impliment search page movement actions

--- client/src/actions/films.js
+++ client/src/actions/films.js
@@ -1,11 +1,14 @@
 import * as api from '../api';
+import { setFilters } from './filters';
 
 export const getFilms = (parameters={}) => async (dispatch) => {
     try {
         const { data } = await api.getFilms(parameters);
+        let { results, total_pages, page } = data;
+        dispatch(setFilters("SET_PAGE", page));
         dispatch({
             type: 'FETCH_SEARCH',
-            payload: data
+            payload: {results, total_pages},
         });
     } catch (error) {
         console.log(error.message);
--- server/filmApi/index.js
+++ server/filmApi/index.js
@@ -1,23 +1,31 @@
 import axios from "axios";
+import console from "console";
+import clone from 'just-clone';
 import { createRequire } from "module"; 
 const require = createRequire(import.meta.url);
 const apiKey = require("./api-keys.json");
 
-// Default film search parameters
+// Default search parameters of the API
 const emptyFilters = {
     country: 'us',
     services: 'netflix',
     type: 'movie',
     order_by: 'imdb_rating',
+    page: {
+		range: [1, 2],
+		startIndex: 0,
+		endIndex: 25,
+		size: 25,
+		direction: 0
+	},
 
     year_min: '1950',
     year_max: new Date().getFullYear(),
 
-    page: '1',
-    desc: 'true',
+    desc: 'false',
 
     genres: '',  
-    genres_relation: 'or',
+    genres_relation: 'and',
 
     min_imdb_rating: '0',
     max_imdb_rating: '100',
@@ -29,17 +37,152 @@ const emptyFilters = {
     keyword: ''
 };
 
+const parseParameters = (parameters) => {
+	// Disregard default optional parameters to simplify query
+	for (let key of Object.keys(emptyFilters).slice(5)) {
+		if (parameters[key] === emptyFilters[key]) {
+			delete parameters[key]
+		}
+	}
+
+	// Parse the JSON string into an object
+	parameters.page = JSON.parse(parameters.page)
+	
+	return parameters
+};
+
 // Fetch single page of films
 export const fetchFilms = (customOptions) => {
     return axios.request({
             method: 'GET',
             url: 'https://streaming-availability.p.rapidapi.com/search/ultra',
-            params: customOptions,
+            params: handleParameters(customOptions),
             headers: {
               'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com',
               'X-RapidAPI-Key': apiKey['filmKey']
             }
     });
+};  
+
+export const fetchFilmsDynamic = async (customOptions) => {
+	const parameters = parseParameters(customOptions);
+	var tempParameters = clone(parameters);
+	var request = {
+		method: 'GET',
+		url: 'https://streaming-availability.p.rapidapi.com/search/ultra',
+		params: parameters,
+		headers: {
+		  'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com',
+		  'X-RapidAPI-Key': apiKey['filmKey']
+		}
+	};
+
+	var totalResults = []
+	switch (parameters.page.direction) {
+		case 0:
+			// Fetch and group page results
+			let range = parameters.page.range;
+			for (let page = range[0]; page < range[1]; page++) {
+				// Update parameters and send request
+				tempParameters.page = page;
+				request.params = tempParameters;
+				let response = await axios.request(request);
+				var { results, total_pages } = response.data;
+
+				// Push results
+				totalResults.push(...results);
+			}
+
+			// slice page to size
+			totalResults = totalResults.slice(
+				parameters.page.startIndex,
+				(totalResults.length - 25 + parameters.page.endIndex)
+			);
+
+			var newPageParam = parameters.page;
+			break;
+
+		case 1:
+			var currentPage = parameters.page.endIndex === 25? 
+				parameters.page.range[1]: 
+				parameters.page.range[1] -1;
+
+			do {
+				// Update parameters and send request
+				tempParameters.page = currentPage;
+				request.params = tempParameters;
+				let response = await axios.request(request);
+				var { results, total_pages } = response.data;
+
+				totalResults.push(...results);
+				currentPage++;
+				
+			} while (
+				totalResults.length - (
+					parameters.page.endIndex === 25? 0: parameters.page.endIndex
+				) !== parameters.page.size
+				&& currentPage !== total_pages
+			);
+
+			// slice page to size
+			totalResults = totalResults.slice(
+				//Start from where the last page left off
+				parameters.page.startIndex,
+				(totalResults.length - 25 + parameters.page.endIndex)
+			);
+
+			// setup replacement page
+			var newPageParam = clone(parameters.page);
+			newPageParam.range = [parameters.page.range[1], currentPage];
+			newPageParam.direction = 0;
+			console.log(newPageParam);
+			break;
+
+		case -1:
+			var currentPage = parameters.page.startIndex === 0? 
+				parameters.page.range[0] - 1: 
+				parameters.page.range[0];
+
+			do {
+				// Update parameters and send request
+				tempParameters.page = currentPage;
+				request.params = tempParameters;
+				let response = await axios.request(request);
+				var { results, total_pages } = response.data;
+
+				totalResults.unshift(...results);
+				currentPage--;
+				
+			} while (
+				totalResults.length - (
+					parameters.page.startIndex === 0? 0: 
+					25 - parameters.page.startIndex
+				) !== parameters.page.size
+				&& currentPage !== 0
+			);
+
+			// slice page to size
+			totalResults = totalResults.slice(
+				//Start from where the last page left off
+				parameters.page.startIndex,
+				(totalResults.length - 25 + parameters.page.endIndex)
+			);
+
+			// setup replacement page
+			var newPageParam = clone(parameters.page);
+			newPageParam.range = [currentPage, parameters.page.range[0]];
+			newPageParam.direction = 0;
+			console.log(newPageParam);
+			break;
+	
+		default:
+			break;
+	};
+	return {
+		results: totalResults, 
+		total_pages: total_pages, 
+		page: newPageParam
+	};
 };  
 
 // Fetch Countries data from API

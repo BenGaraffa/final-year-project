d35f4203a0e607d84806b643334355d380878db1
BenGaraffa <ben.garaffa@gmail.com>
2022-04-20 22:54:02 +0100

    Update PageController component

--- client/src/components/Films/PageController/PageController.js
+++ client/src/components/Films/PageController/PageController.js
@@ -1,51 +1,41 @@
-import { Button, Stack, Typography } from '@mui/material';
-import React from 'react'
+import { Stack, Pagination, Container } from '@mui/material';
+import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { getFilms, setFilmsEmpty } from '../../../actions/films';
 import { setFilters } from '../../../actions/filters';
 import clone from 'just-clone'
+import { Box } from '@mui/system';
 
 const PageController = ({ films }) => {
     const filters = useSelector((state) => state.filters);
     const { page } = filters;
     const dispatch = useDispatch();
-    
-    const handleNext = () => {
-        let newPage = clone(page)
-        newPage.direction = 1
+    const pageSpan = page.range[1] - page.range[0]
 
-        dispatch(setFilters("SET_PAGE", newPage))
-        dispatch(setFilmsEmpty())
-        dispatch(getFilms(filters))
-    }
-    const handlePrev = () => {
+    const [pagination, setPagination] = useState(page.range[0]);
+    
+    const handleChange = (event, value) => {
+        setPagination(value)
         let newPage = clone(page)
-        newPage.direction = -1
-        
+        newPage.range = [value, value + pageSpan]
+
         dispatch(setFilters("SET_PAGE", newPage))
         dispatch(setFilmsEmpty())
         dispatch(getFilms(filters))
     }
 
     return (
-        <Stack 
-            direction="row"
-            alignItems="center"
-            justifyContent="space-between"
+        <Box
+            display='flex'
+            justifyContent='center'
         >
-            <Button disabled={page.range[0] === 1} onClick={handlePrev}>
-                PREV
-            </Button>
-            <Typography variant="subtitle2">
-                pages {page.range[0]} of {Math.floor(films.total_pages/(page.range[1]-page.range[0]))}
-            </Typography>
-            <Button 
-                disabled={page.range[1] === films.total_pages + 1}
-                onClick={handleNext}
-            >
-                NEXT
-            </Button>
-        </Stack>
+            <Pagination 
+                count={Math.floor(films.total_pages/(page.range[1]-page.range[0]))} 
+                page={pagination} 
+                onChange={handleChange} 
+                sx={{paddingBottom:1, paddingTop:1}}
+            />
+        </Box>
     )
 };
 export default PageController;

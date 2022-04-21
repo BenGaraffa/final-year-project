74c8a29f1d608ef1e3403c5e5f25bc26ddce93de
BenGaraffa <ben.garaffa@gmail.com>
2022-04-21 01:38:12 +0100

    Update film component with button base

--- client/src/components/Films/Film/Film.js
+++ client/src/components/Films/Film/Film.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import { Card, CardContent, CardMedia, Typography } from '@mui/material'
+import { ButtonBase, Card, CardMedia, Typography } from '@mui/material'
 
 const Film = ({ film }) => {
 
     return  (
-        <Card sx={{paddingBottom: 0}}>
-            <CardMedia
-                component="img"
-                height="200"
-                image={film.posterURLs["185"]}
-            />
-            <CardContent sx={{p:0.5}}>
+        <Card>
+            <ButtonBase sx={{width:'100%', height:'100%'}}>
+                <CardMedia
+                    component="img"
+                    height="200"
+                    image={film.posterURLs["185"]}
+                />
+            </ButtonBase>
             <Typography 
                 variant="caption" 
                 component="div"
                 align='center' 
+                padding={1}
             >
                 {film.title}
             </Typography>
-            </CardContent>
         </Card>
     )
 }
